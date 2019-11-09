acd30295b70f4bd9fd8f53924d5bc850f1612b1d
Khalid Hassan Sk <52673145+khalid0007@users.noreply.github.com>
2019-11-09 15:43:34 +0530

    Delete Alphacode.cpp

--- Alphacode.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int solve(const string& a)
-{
-	
-}
-
-int main(){while(true) {string mssg; cin >> mssg;if(mssg[0] == '0') break; else cout << solve(mssg) << "\n";}}
